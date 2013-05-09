943e43ab146937f9bbef376ea14a489e6a4cfe3d
Student <student@ent-v321-010.local>
2013-05-08 20:31:20 -0400

    added python files

---
+++ mad_libs_lyrics.py
@@ -0,0 +1,24 @@
+"""
+mad_libs_lyrics.py
+====
+* using your lyrics from lyrics.py, replace 4 words with variables
+* set your variables equal to strings at the top of the file
+* name the variables after the part of speech of the word they are replacing,
+  followed by a number:
+
+  adjective1 = 'tiny'
+  adjective2 = 'magical'
+  noun1 = 'ukulele'
+
+* print the new lyrics with variables to the console
+"""
+
+noun1 = 'couch'
+noun2 = 'cheese'
+verb1 = 'punch'
+verb2 = 'run'
+
+print("And so I %s sometimes when i'm lying in %s" % (verb1,noun1)) 
+print("Just to %s it all out" % (verb2))
+print("What's in my %s" % (noun2))
+print("And I, I am feeling a little peculiar")
